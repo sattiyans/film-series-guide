6e7776ff821031d5d7f4f847c8ee33b4653f1ff8
sattexe <sattiyan@outlook.com>
2023-04-13 14:38:52 +0800

    Update script.js

--- script.js
+++ script.js
@@ -55,7 +55,7 @@ function loadMovieDetails() {
       searchList.classList.add("hide-search-list");
       movieSearchBox.value = "";
       const result = await fetch(
-        `http://www.omdbapi.com/?i=${movie.dataset.id}&apikey=98138f60`
+        `https://www.omdbapi.com/?i=${movie.dataset.id}&apikey=98138f60`
       );
       const movieDetails = await result.json();
       // console.log(movieDetails);
